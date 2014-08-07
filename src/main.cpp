de12efc1d1c970911dc3746c1fe605f8b4755de5
Brunna <brunnaamorim@gmail.com>
2014-08-07 02:58:08 -0700

    final version

--- src/main.cpp
+++ src/main.cpp
@@ -7,30 +7,65 @@
 
 using namespace std;
 
+//Search for '#' in str[] (user input)
 void comment(char str[], int n) {
 	for (int i = 0; i < n; i++) {
 		if (str[i] == '#') {
-			str[i] = '\0';
+			str[i] = '\0'; 
 		}
 	}
 }
 
+//Search for '&' in str[] (user input)
+bool background(char str[], int n) {
+	bool bg = false;
+	for (int i = 0; i < n; i ++) {
+		if (str[i] == '&') {
+			bg = true;
+		}
+	}
+	return bg;
+}
+
 void prompt(char str[], int n) {
-	 char name[50];
-         gethostname(name, sizeof name);
+	char host[50];
+        if ((gethostname(host, sizeof(host)-1))==-1) {
+		host[0] = 'h';
+		host[1] = 'o';
+		host[2] = 's';
+		host[3] = 't';
+		host[4] = '\0';
+//		perror("hostname error");
+	}
 
-         cout << getlogin() << "@" << name << "$ ";
+	char login[50];
+	
+	if (getlogin_r(login, sizeof(login)-1)-1) {
+		login[0] = 'u';
+		login[1] = 's';
+		login[2] = 'r';
+		login[3] = '\0';
+//		perror("user error");
+	}
+	cout << login << "@" << host << "$ ";
 
-         cin.getline(str,n);
+        cin.getline(str,n);
 
-         while (str[0] == '\0' || str[0] == '#') {
-                 cout << getlogin() << "@" << name << "$ ";
-                 cin.getline(str,n);
+	//Check if there is '&' in the input. If its true, the prompt will be called again
+	if(background(str, n) == true) {
+		cout << login  << "@" << host << "$ ";
+		cin.getline(str,n);
+	}
+
+	//Check if some comment has been made in the input
+        while (str[0] == '\0' || str[0] == '#') {
+                cout << login << "@" << host << "$ ";
+                cin.getline(str,n);
 	}										 
 }
 
-int main(int argc, char** argv) {
 
+int main(int argc, char** argv) {
 
 while (true) {
 
@@ -47,6 +82,7 @@ while (true) {
 
 	comment(str, 256);
 
+	//Initialize *result[] with the user input, splited by spaces 
 	while (pch != NULL)
 	{
 	 	result[index] = pch;
@@ -56,13 +92,12 @@ while (true) {
 
 	result[index] = NULL;
 	
-	
+	//Exits the rshell
 	if (strcmp(result[0], "exit") == 0) {
-
 		exit(0);
 	}
 
-	
+	//Executes the command
 	int pid = fork();
 	if (pid == 0) {
 		if (execvp(result[0], result) == -1) {
